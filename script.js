75c1204ac11a2b4ef2a6cbcdb03e31247226b861
CharlotteH86 <70633528+CharlotteH86@users.noreply.github.com>
2020-10-18 14:10:59 +0200

    Added files

---
+++ script.js
@@ -0,0 +1,30 @@
+let popped = 0;
+const balloons = document.querySelectorAll('.balloons');
+const once = {
+    once : true
+}
+
+balloons.forEach((element) => {
+    element.addEventListener('mouseover', (event) =>{
+        event.target.style.backgroundColor = "lightgrey";
+        event.target.textContent = "POP!"
+        popped++;
+        //event.target.classList.add("pop");
+        //removeEvent(event);
+        checkAllPopped();
+        
+    }, once)
+})
+
+function removeEvent (event){
+    event.target.removeEventListener('mouseover', () =>{})
+}
+
+function checkAllPopped(){
+    if(popped === balloons.length){
+        const message = document.querySelector('#whenPopped');
+        const gallery = document.querySelector('#balloons-gallery')
+        gallery.innerHTML='';
+        message.style.display = 'block';
+    }
+}
