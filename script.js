627376a7906eca2a3647e5a9dddd584b293d813f
CharlotteH86 <70633528+CharlotteH86@users.noreply.github.com>
2020-10-22 11:39:32 +0200

    Removed function and added comments

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
+//Variabler som snappar upp .balloons. Once innebär att t.ex. forEach nedan bara aktiveras en gång.
 let popped = 0;
-const balloons = document.querySelectorAll('.balloons');
+const balloons = document.querySelectorAll(".balloons");
 const once = {
-    once : true
-}
+  once: true,
+};
 
+//Vid 'mouseover' ändras bagrundsfärgen på 'ballongen' och texten "POP!" skapas.
 balloons.forEach((element) => {
-    element.addEventListener('mouseover', (event) =>{
-        event.target.style.backgroundColor = "lightgrey";
-        event.target.textContent = "POP!";
-        popped++;
-        checkAllPopped();
-    }, once)
-})
+  element.addEventListener(
+    "mouseover",
+    (event) => {
+      event.target.style.backgroundColor = "lightgrey";
+      event.target.textContent = "POP!";
+      popped++;
+      checkAllPopped();
+    },
+    once
+  );
+});
 
-function removeEvent (event){
-    event.target.removeEventListener('mouseover', () =>{})
-}
-
-function checkAllPopped(){
-    if(popped === balloons.length){
-        const message = document.querySelector('#whenPopped');
-        const gallery = document.querySelector('#balloons-gallery')
-        gallery.innerHTML='';
-        message.style.display = 'block';
-    }
+//Denna funktion skapas när alla ballonger 'smällt'. Texten under whenPopped visas och 'galleriet' meed ballonger visas tomt.
+function checkAllPopped() {
+  if (popped === balloons.length) {
+    const message = document.querySelector("#whenPopped");
+    const gallery = document.querySelector("#balloons-gallery");
+    gallery.innerHTML = "";
+    message.style.display = "block";
+  }
 }
